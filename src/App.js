daf4911802e83962da66f94982e170f3cbf3dc22
こにー | Connie <71988605+Connie0914@users.noreply.github.com>
2021-10-15 15:44:48 +0900

    study useCallBack

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   const [open, setOpen] = useState(false);
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), []);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
@@ -15,7 +15,6 @@ export default function App() {
       <br />
       <button onClick={onClickOpen}>表示</button>
       <ChildArea open={open} onClickClose={onClickClose} />
-      <p>{text}</p>
     </div>
   );
 }
